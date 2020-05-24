3d5b7ec675550cd025dabe5c35536fb958679a0f
rafaelfs96 <rafaelfs96@outlook.com>
2020-05-24 16:48:24 -0300

    pwa

--- pwa/public/src/js/app.js
+++ pwa/public/src/js/app.js
@@ -1,5 +1,7 @@
 let deferredPrompt
 
+if(!window.Promise) window.Promise = Promise
+
 if('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js' /*, {scope: '/help/'}*/).then(function() {
 		console.log('sw.js registered')
