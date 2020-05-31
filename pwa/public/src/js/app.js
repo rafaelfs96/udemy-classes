55328f126bb52954e0afd9a43c9957d9d43ab29a
rafaelfs96 <rafaelfs96@outlook.com>
2020-05-31 14:15:58 -0300

    PWA: caching static and dynamic files

--- pwa/public/src/js/app.js
+++ pwa/public/src/js/app.js
@@ -3,9 +3,14 @@ let deferredPrompt
 if(!window.Promise) window.Promise = Promise
 
 if('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js' /*, {scope: '/help/'}*/).then(function() {
-		console.log('sw.js registered')
-	})
+	navigator.serviceWorker
+		.register('/sw.js' /*, {scope: '/help/'}*/)
+		.then(function() {
+			console.log('sw.js registered')
+		})
+		.catch(function(err) {
+			console.log(err)
+		})
 }
 
 window.addEventListener('beforeinstallprompt', function(event) {
