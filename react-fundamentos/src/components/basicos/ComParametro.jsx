be0d625b962c4d52ffbc86afc56c9ac03f12ff68
rafaelfs96 <rafaelfs96@outlook.com>
2020-07-25 23:10:49 -0300

    react udemy

---
+++ react-fundamentos/src/components/basicos/ComParametro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ComParametro(props) {
+  console.log(props)
+  return (
+    <div>
+      <h2>{ props.titulo }</h2>
+      <p>{ props.subtitulo }</p>
+    </div>
+  )
+}
