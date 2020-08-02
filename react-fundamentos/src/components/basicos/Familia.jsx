126727d308412bcea3e25745a675922238d4744d
rafaelfs96 <rafaelfs96@outlook.com>
2020-08-02 17:19:00 -0300

    move projects to new repositories

--- react-fundamentos/src/components/basicos/Familia.jsx
+++
@@ -1,9 +0,0 @@
-import React, { cloneElement } from 'react'
-
-export default props => {
-  return (
-    <div>
-      { props.children.map((child, index) => cloneElement(child, { ...props, key: index })) }
-    </div>
-  )
-}
