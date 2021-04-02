f9437527b9531a1733d951b224160c6507fb387f
rafaelfs96 <rafaelfs96@outlook.com>
2021-04-02 14:16:14 -0300

    add firebase and indexeddb

--- pwa/public/sw.js
+++ pwa/public/sw.js
@@ -1,4 +1,6 @@
-var CACHE_STATIC_NAME = 'static-v15';
+importScripts('/src/js/idb.js')
+
+var CACHE_STATIC_NAME = 'static-v16';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
   '/',
@@ -6,6 +8,7 @@ var STATIC_FILES = [
   '/offline.html',
   '/src/js/app.js',
   '/src/js/feed.js',
+  '/src/js/idb.js',
   '/src/js/promise.js',
   '/src/js/fetch.js',
   '/src/js/material.min.js',
@@ -16,6 +19,12 @@ var STATIC_FILES = [
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
+
+var dbPromise = idb.open('posts-store', 1, function (db) {
+  if(!db.objectStoreNames.contains('posts')) {
+    db.createObjectStore('posts', { keyPath: 'id' })
+  }
+})
 
 // function trimCache(cacheName, maxItems) {
 //   caches.open(cacheName)
@@ -70,19 +79,26 @@ function isInArray(string, array) {
 
 self.addEventListener('fetch', function (event) {
 
-  var url = 'https://httpbin.org/get';
+  var url = 'https://pwagram-a8-default-rtdb.firebaseio.com/posts';
   if (event.request.url.indexOf(url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-        .then(function (cache) {
-          return fetch(event.request)
-            .then(function (res) {
-              // trimCache(CACHE_DYNAMIC_NAME, 3);
-              cache.put(event.request, res.clone());
-              return res;
-            });
-        })
-    );
+    event.respondWith(fetch(event.request)
+      .then(function(res) {
+        var cloned = res.clone()
+        cloned.json()
+          .then(function(data) {
+            for(var key in data) {
+              dbPromise
+                .then(function(db) {
+                  var tx = db.transaction('posts', 'readwrite')
+                  var store = tx.objectStore('posts')
+                  store.put(data[key])
+                  return tx.complete
+                })
+            }
+          })
+        return res
+      })
+    )
   } else if (isInArray(event.request.url, STATIC_FILES)) {
     event.respondWith(
       caches.match(event.request)
